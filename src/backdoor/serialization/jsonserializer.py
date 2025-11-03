ee6c47e832d1dd313dc293ff0f8e519f6531ad6d
Ramon Saviato <ramonsaviato@hotmail.com>
2025-11-03 13:32:56 -0300

    fix: make JsonSerializer implement Serializer

--- src/backdoor/serialization/jsonserializer.py
+++ src/backdoor/serialization/jsonserializer.py
@@ -4,9 +4,10 @@ from typing import Any
 from backdoor.serialization.encoders.aggregator import EncoderAggregator
 from backdoor.serialization.decoders.aggregator import DecoderAggregator
 from backdoor.serialization.exceptions import BadDataError
+from backdoor.serialization.serializer import Serializer
 
 
-class JsonSerializer:
+class JsonSerializer(Serializer):
 
     def serialize(self, payload: Any) -> bytes:
         return json.dumps(payload, cls=EncoderAggregator).encode()
