39a3e687a90dc6d29a17dd071be1d2dfc1b1dd36
Ramon Saviato <ramonsaviato@hotmail.com>
2025-11-18 13:33:06 -0300

    fix: add freeze_support() call on client
    Due to psutil lib usage, multiple wmic processes were being executed on windows
    environment. Calling freeze_support() fixes that.

--- src/backdoor/client.py
+++ src/backdoor/client.py
@@ -1,4 +1,5 @@
 from argparse import ArgumentParser, Namespace
+import multiprocessing
 import socket
 import time
 
@@ -89,4 +90,5 @@ def main() -> None:
 
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     main()
